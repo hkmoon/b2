a2e1655b4c297ed92f5c3904b266f58431d244ff
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2025-08-13 21:22:04 -0400

    simplified example code

--- python/examples/solve_system.py
+++ python/examples/solve_system.py
@@ -1,33 +1,19 @@
 import bertini as pb
-import numpy as np
 
 x = pb.Variable('x')
 y = pb.Variable('y')
-
-vg = pb.VariableGroup([x,y])
 
 sys = pb.System()
 
 sys.add_function(x-y)
 sys.add_function(x**2 + y**2 - 1)
 
-sys.add_variable_group(vg)
-
-C = pb.multiprec.Complex
-
-variable_values = np.array([C(0), C(0)])
-
-result = sys.eval(variable_values)
-print(result)
-
+sys.add_variable_group(pb.VariableGroup([x,y]))
 sys.homogenize()
 sys.auto_patch()
 
-
 solver = pb.nag_algorithm.ZeroDimCauchyAdaptivePrecisionTotalDegree(sys)
-
 solver.solve()
 
 for soln in solver.solutions():
-
 	print(sys.dehomogenize_point(soln))
